756dbc882da4ef272b8b4d59d0eec9e5b6e27f8f
Anna Pankratova <153401722+wimtdw@users.noreply.github.com>
2025-05-13 16:19:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,16 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py update_book_url
+python manage.py shell -c "
+from books.models import Background
+try:
+    obj = Background.objects.get(id=5)
+    obj.url = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/fcee6692300971.5e47d656aae59.jpg'
+    obj.save()
+    print('Updated URL for entry 5')
+except BooksBackground.DoesNotExist:
+    print('Entry 5 not found')
+"
 # python manage.py loaddata initial_data.json
 
 # if [[ $CREATE_SUPERUSER ]]
