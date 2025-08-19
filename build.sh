89e4bfc917b963e96447c407ad3ff4c300d7bc45
Anna Pankratova <153401722+wimtdw@users.noreply.github.com>
2025-08-19 16:28:07 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,9 +17,9 @@ python manage.py migrate
 #     print('Entry 5 not found')
 # "
 
-# python manage.py loaddata initial_data.json
+python manage.py loaddata initial_data.json
 
-# if [[ $CREATE_SUPERUSER ]]
-# then
-#     python manage.py createsuperuser --no-input
-# fi
+if [[ $CREATE_SUPERUSER ]]
+then
+    python manage.py createsuperuser --no-input
+fi
