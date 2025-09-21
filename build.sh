b9930f791ee94a2d5fe188062e26634b76dc3334
Anna Pankratova <153401722+wimtdw@users.noreply.github.com>
2025-09-21 18:44:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,18 +6,18 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-# python manage.py shell -c "
-# from books.models import Background
-# try:
-#     obj = Background.objects.get(id=5)
-#     obj.url = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/fcee6692300971.5e47d656aae59.jpg'
-#     obj.save()
-#     print('Updated URL for entry 5')
-# except BooksBackground.DoesNotExist:
-#     print('Entry 5 not found')
-# "
+python manage.py shell -c "
+from books.models import Background
+try:
+    obj = Background.objects.get(id=3)
+    obj.url = 'https://i.pinimg.com/originals/75/f7/a3/75f7a3732d6953e9ab5d437ff851721b.jpg'
+    obj.save()
+    print('Updated URL for entry 3')
+except BooksBackground.DoesNotExist:
+    print('Entry 3 not found')
+"
 
-python manage.py loaddata initial_data.json
+# python manage.py loaddata initial_data.json
 
 # if [[ $CREATE_SUPERUSER ]]
 # then
